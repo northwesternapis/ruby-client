a482d8076338bdfa5449113720dd331f71c54f32
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-10-04 14:54:13 -0500

    switched to class methods

--- lib/northwestern.rb
+++ lib/northwestern.rb
@@ -6,53 +6,53 @@ module Northwestern
   BASE_URL = 'https://api.asg.northwestern.edu/'
   class << self
     def terms(params={})
-      get('/terms', {})
+      self.get('/terms', {})
     end
 
     def schools(params={})
-      get('/schools', {})
+      self.get('/schools', {})
     end
 
     def terms(params={})
-      get('/terms', {})
+      self.get('/terms', {})
     end
 
     def subjects(params={})
-      get('/subjects', params)
+      self.get('/subjects', params)
     end
 
     def courses(params={})
-      get('/courses', params)
+      self.get('/courses', params)
     end
 
     def course_details(params={})
-      get('/courses/details', params)
+      self.get('/courses/details', params)
     end
 
     def instructors(params={})
-      get('/instructors', params)
+      self.get('/instructors', params)
     end
 
     def rooms(params={})
-      get('/rooms', params)
+      self.get('/rooms', params)
     end
 
     def room_details(params={})
-      get('/rooms/details', params)
+      self.get('/rooms/details', params)
     end
-  end
 
-  def get(path, params)
-    if defined? Config::API_KEY
-      uri = URI(BASE_URL)
-      uri.path = path
-      uri.query = URI.encode_www_form(params.merge(
-        { key: Config::API_KEY }
-      ))
+    def get(path, params)
+      if defined? Northwestern::Config::API_KEY
+        uri = URI(BASE_URL)
+        uri.path = path
+        uri.query = URI.encode_www_form(params.merge(
+          { key: Northwestern::Config::API_KEY }
+        ))
 
-      JSON.parse(Net::HTTP.get(uri))
-    else
-      "Please set your API key (see readme)"
+        JSON.parse(Net::HTTP.get(uri))
+      else
+        "Please set your API key (see readme)"
+      end
     end
   end
 end
